17b06f780477c6c30ad0ca4284bbeede533efe86
Pradip Pandey <pra.baduwal3@gmail.com>
2025-01-08 15:01:33 +0545

    Added footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className='h-[100vh] w-full bg-theme-primary text-gray-300'>
       <Header />
+      <Footer />
     </div>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='h-[10vh] flex justify-center items-center bg-theme-seconday text-xl text-gray-400'>
+        <h1>Copyright | All rights reserved.</h1>
+    </div>
+  )
+}
+
+export default Footer;
