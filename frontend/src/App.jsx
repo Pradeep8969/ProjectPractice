e460de2bf58533dce1b74b69413ba8de19c6ff0d
shirishstha <sthasiriz123@gmail.com>
2025-01-08 15:01:04 +0545

    Intro added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './components/Header';
+import Intro from './components/Into'
 
 const App = () => {
   return (
     <div className='h-[100vh] w-full bg-theme-primary text-gray-300'>
       <Header />
+      <Intro/>
+
     </div>
   )
 }
---
+++ frontend/src/components/Into.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Into() {
+  return (
+    <div>
+     <h1>Hello this is shirish test</h1> 
+    </div>
+  )
+}
